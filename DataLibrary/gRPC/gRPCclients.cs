ad4f2f768adfe3a576a2b4939d683a61a23280fc
Dariusz Tomczak <dktomczak@o2.pl>
2021-11-21 00:10:19 +0100

    Add project files.

---
+++ DataLibrary/gRPC/gRPCclients.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Net.Client;
+using GrprcServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GrprcServer.Greeter;
+
+namespace DataLibrary.gRPC
+{
+    public class gRPCclients
+    {
+        #region Singelton
+        private static readonly Lazy<gRPCclients> lazy =
+        new Lazy<gRPCclients>(() => new gRPCclients());
+        public static gRPCclients GetInstance { get { return lazy.Value; } }
+        private gRPCclients()
+        {
+          
+        }
+        #endregion
+        #region Fields
+        private string _ServerIP= @"http://localhost:5000";
+        #endregion
+        #region Methods
+        public string TestgRPCConnection(string userName)
+        {
+            string res;
+            using (var channel = GrpcChannel.ForAddress(_ServerIP))
+            {
+                var client = new GreeterClient(channel);
+                var input = new HelloRequest() { Name = userName };
+                res = client.SayHello(input).Message;
+            }
+            return res;
+        }
+        #endregion
+    }
+}
---
+++ TestGRPC/Program.cs
@@ -0,0 +1,16 @@
+﻿using DataLibrary.gRPC;
+using System;
+using static System.Console;
+
+namespace TestGRPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverContext = gRPCclients.GetInstance;
+            WriteLine(serverContext.TestgRPCConnection("Darek"));
+            ReadKey();
+        }
+    }
+}
